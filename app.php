07af67d9d6e291c2c4e6221243e614964ffa0384
Pekhov14 <pehovanton21@mail.com>
2021-03-10 22:55:58 +0200

    create folders

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 /*
  * TODO:
  * Подчитать колличевство файлов
@@ -18,9 +18,9 @@ class TranslateFolder {
     private $pattern    = "/\s'(.+?)'/";
     private $outputFile = './files/category_out.php';
 
-    private $tr;
+	private $tr;
 
-    public function __construct($params)
+	public function __construct($params)
     {
         if(!isset($params[1]) || !isset($params[2])) {
             trigger_error('My creator wants you to pass two arguments');
@@ -33,39 +33,33 @@ class TranslateFolder {
     }
 
     public function run($dirName, $newDir) {
-        mkdir($newDir, 0777, true);
+		if (!is_dir(__DIR__ . '/files/' . $newDir)) {
+			mkdir(__DIR__ . '/files/' . $newDir, 0777, true);
+		}
 
 		$di = new RecursiveDirectoryIterator($dirName,RecursiveDirectoryIterator::SKIP_DOTS);
 		$it = new RecursiveIteratorIterator($di);
 
-//        foreach($it as $fileinfo){
-//		    var_dump($it->getSubPathName());
-//		    var_dump($it->getSubPath());
-//        }
-//
-//		die();
-
-//        echo $dirName . ' ' . $newDir .  PHP_EOL;
         $start = microtime(true);
 
 		$i = 0;
 		$hashes = $this->generateDots();
 
 		foreach($it as $file) {
-			if (pathinfo($file, PATHINFO_EXTENSION) === "php") {
-//				echo $file, PHP_EOL;
+			$fileInfo = pathinfo($file);
 
-                // Передать имя путь и имя файла
-                 $this->translateFile($file);
+			if ($fileInfo['extension'] === "php") {
+				$output = [];
+				$output[] = 'Working: ' . $i  . '% ' . $hashes;
+				$output[] = 'Please wait while our monkeys finish translating';
 
-                $output = [];
-                $output[] = 'Working: ' . $i  . '% ' . $hashes;
-                $output[] = 'Please wait while our monkeys finish translating';
+				$this->replaceCommandOutput($output);
 
-                $this->replaceCommandOutput($output);
-                // sleep(1);
-                usleep(100000);
-                $hashes[$i] = '#';
+				usleep(100000);
+				$hashes[$i] = '#';
+
+                $this->translateFile($it->getSubPath(), $fileInfo['basename'], $it->getSubPathName());
+
                 $i++;
             }
         }
@@ -94,24 +88,26 @@ class TranslateFolder {
         return $hashs;
     }
 
-    private function translateFile($pathToFile) {
-        foreach($this->getLines($pathToFile) as $line) {
+    private function translateFile($pathToFile, $nameFile, $fullName) {
+
+		// TODO: решить проблему с путем '/files/ru-ru/'
+        foreach($this->getLines(__DIR__ . '/files/ru-ru/' . $fullName) as $line) {
+
             preg_match($this->pattern, $line, $matches);
 
             $resultLine = $line;
 
             if($matches) {
                 $translate = ' ';
-                $translate .= $this->tr->translate($this->source, $this->target, $matches[0], $this->attempts);
+                $translate .= $this->tr::translate($this->source, $this->target, $matches[0], $this->attempts);
                 $resultLine = preg_replace($this->pattern, $translate, $line);
             }
 
-
-            if (!is_dir(__DIR__ . '/files/ua-ua/7777')) {
-                mkdir(__DIR__ . '/files/ua-ua/7777', 0777);
+            if (!is_dir(__DIR__ . '/files/ua-ua/' . $pathToFile)) {
+                mkdir(__DIR__ . '/files/ua-ua/' . $pathToFile, 0777, true);
             }
 
-                file_put_contents(__DIR__ . '/files/ua-ua/7777/test.txt', $resultLine, FILE_APPEND | LOCK_EX);
+            file_put_contents(__DIR__ . '/files/ua-ua/' . $pathToFile . '/'. $nameFile, $resultLine, FILE_APPEND | LOCK_EX);
         }
     }
 
